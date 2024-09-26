4c9d417eefe5714c2a684afe5cff9e302cc4f7cc
Evgene V <evgenevolkov@users.noreply.github.com>
2024-09-26 13:44:40 +0200

    add file encoding

--- prices_analyzer/app/utils/utils.py
+++ prices_analyzer/app/utils/utils.py
@@ -10,7 +10,7 @@ logger = get_logger(__name__)
 def load_yaml_file(file_path: str) -> dict:
     """gracefully reads and parses yaml file"""
     try:
-        with open(file_path, 'r') as file:
+        with open(file_path, 'r', encoding='utf-8') as file:
             return yaml.safe_load(file)
     except FileNotFoundError:
         logger.error(f"Config file not found: {file_path}")
